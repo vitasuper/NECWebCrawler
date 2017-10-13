f1e2f668cac73e2cb4450503f2c59cb8ec325d5c
vitasuper <vitasuperh@gmail.com>
2017-10-12 23:52:29 -0700

    Modified HtmlFetcher.

--- src/main/java/com/longyue/crawler/HtmlFetcher.java
+++ src/main/java/com/longyue/crawler/HtmlFetcher.java
@@ -13,7 +13,14 @@ public class HtmlFetcher {
         }
         try {
             Connection.Response response = Jsoup.connect(url).timeout(3000).execute();
-            return response.body();
+
+            // System.out.println(response.body());  // Get the body of the HTML page
+
+            // Recommended judging process
+            return response.statusCode() / 100 == 2 ? response.body() : null;
+
+            // return response.body();
+
         } catch (IOException e) {
             System.out.println(e);
             return null;
@@ -22,7 +29,7 @@ public class HtmlFetcher {
 
     public static <T> void main(String[] args) throws Exception {
         HtmlFetcher htmlFetcher = new HtmlFetcher();
-        System.out.println(htmlFetcher.fetch("https://music.163.com/#/discover/playlist"));
+        System.out.println(htmlFetcher.fetch("http://music.163.com/#/discover/playlist/?order=hot&cat=%E5%85%A8%E9%83%A8&limit=35&offset=0"));
     }
 
 }
